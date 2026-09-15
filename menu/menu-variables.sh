8b9e72e8d514cee0f723d2117be5706b24544e12
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-09-15 14:21:58 +0200

    Add menu options declaration to menu-variables.sh

--- menu/menu-variables.sh
+++ menu/menu-variables.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 # ------------------------------------------------------------------------------
-# Desktop Environment
+# Menu Variables
 # ------------------------------------------------------------------------------
 # /menu/menu-variables.sh
 
 # Path Variables
 MENU_MODULES="$MENU_ROOT/modules"
+
+# Menu Options
+declare -A MENU_OPTIONS
 
 # Package Variables
 MENU_DESKTOP_ENV="hyprland"
