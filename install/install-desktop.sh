608bfef7c7b63e6e78dda89df09733210b181c83
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-09-15 16:51:16 +0200

    Implement desktop environment installation script
    Add a function to install desktop environments based on user selection.

--- install/install-desktop.sh
+++ install/install-desktop.sh
@@ -1,1 +1,28 @@
+#!/usr/bin/env bash
 
+# ------------------------------------------------------------------------------
+# Desktop Installation
+# ------------------------------------------------------------------------------
+# /install/install-desktop.sh
+
+install_desktop()
+{
+    INSTALL_PACKAGES=""
+
+    case "$MENU_DESKTOP_ENV" in
+        hyprland)
+            INSTALL_PACKAGES+=" hyprland"
+            ;;
+        plasma)
+            INSTALL_PACKAGES+=" plasma"
+            ;;
+        gnome)
+            INSTALL_PACKAGES+=" gnome"
+            ;;
+        skip)
+            return
+            ;;
+    esac
+
+    sudo pacman -S --needed $INSTALL_PACKAGES
+}
