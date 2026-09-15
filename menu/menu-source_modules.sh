e4244f639f24a14e051b41f98f0776b08b478a8a
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-09-15 14:15:56 +0200

    Update menu-source_modules.sh

--- menu/menu-source_modules.sh
+++ menu/menu-source_modules.sh
@@ -4,5 +4,5 @@
 # Menu Module Loader
 # ------------------------------------------------------------------------------
 # /menu/menu-source_modules.sh
-
+source "$MENU_MODULES/menu-select_custom.sh"
 source "$MENU_MODULES/menu-environment.sh"
