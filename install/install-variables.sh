5dd662015b40684ca02ce76de5738e57c04caf65
Willem Achterhof <79074811+WillemAchterhof@users.noreply.github.com>
2026-09-15 16:42:39 +0200

    Create install-variables.sh with basic structure
    Added initial structure and comments for install-variables.sh

--- install/install-variables.sh
+++ install/install-variables.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
 
+# ------------------------------------------------------------------------------
+# Install Variables
+# ------------------------------------------------------------------------------
+# /install/install-variables.sh
+
+# Path Variables
+
+# Menu Options
+
+# Package Variables
+INSTALL_PACKAGES=""
