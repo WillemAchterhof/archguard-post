3c2f96c2bb28b4016cace077710f22e93bf7e1b6
WillemAchterhof <willem.achterhof@protonmail.com>
2026-09-15 20:36:30 +0200

    Added install

--- install/install-desktop.sh
+++ install/install-desktop.sh
@@ -7,9 +7,13 @@
 
 install_desktop()
 {
-    INSTALL_PACKAGES+=" $MENU_DESKTOP_ENV"
+    INSTALL_PACKAGES+="$MENU_DESKTOP_ENV"
+
+    if [[ "$MENU_DESKTOP_ENV" == "hyprland" ]]; then
+        INSTALL_PACKAGES+=" $(<"$ROOT_DESKTOP/hyprland/packages.env")"
+    fi
 
     INSTALL_PACKAGES="${INSTALL_PACKAGES// skip/}"
 
-    sudo pacman -S --needed $INSTALL_PACKAGES
+    sudo pacman -S --needed --noconfirm $INSTALL_PACKAGES
 }
