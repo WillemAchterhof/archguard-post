709f0bbcf24e78d37c9229573b3c2862b9ceee03
WillemAchterhof <willem.achterhof@protonmail.com>
2026-09-20 01:05:50 +0200

    background included

--- config/restore-configs.sh
+++ config/restore-configs.sh
@@ -16,12 +16,16 @@ restore_configs()
 {
     local repository="$HOME/.archguard/archguard-configs"
     local config_file="$repository/backup-configs.env"
+    local background="$HOME/Pictures/BackGround"
 
     printf "[*] Restoring ArchGuard configs...\n"
 
     restore_copy \
         "$config_file" \
         "$repository"
+
+    mkdir -p -- "$background"
+    cp /opt/archguard/backup/ArchGuard.png "$background/ArchGuard.png"
 
     printf "[*] ArchGuard configs restored.\n"
 }
