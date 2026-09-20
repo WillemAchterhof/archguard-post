03f64eba86caba2b3460ac92d75117923f7613e3
WillemAchterhof <willem.achterhof@protonmail.com>
2026-09-20 11:03:41 +0200

    f

--- root-run.sh
+++ root-run.sh
@@ -20,7 +20,6 @@ source "$ROOT_MENU/menu-run.sh"
 source "$ROOT_INSTALL/install-run.sh"
 source "$ROOT_CONFIG/config-run.sh"
 source "$ROOT_FINISH/finish-run.sh"
-source "$AG_HOME/.archguard/lib/restore-configs"
 
 
 # Run
