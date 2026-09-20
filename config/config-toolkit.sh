68b7ed746d945bdbbcecddee3cbacab7180f5fce
WillemAchterhof <willem.achterhof@protonmail.com>
2026-09-20 11:52:24 +0200

    test

--- config/config-toolkit.sh
+++ config/config-toolkit.sh
@@ -6,36 +6,21 @@
 # /config/config-toolkit.sh
 # ------------------------------------------------------------------------------
 
-initialize()
-{
-    printf '[*] Initializing ArchGuard...\n'
-    printf '[*] User: %s\n' "$AG_USER"
-    printf '[*] Home: %s\n' "$AG_HOME"
-
-    local toolkit="$AG_HOME/.archguard/toolkit"
-
-    source "$toolkit/archguard-toolkit.sh"
-    source "$toolkit/backup-configs/backup-configs.env"
-    source "$toolkit/lib/restore-copy.sh"
-
-    printf '[*] Initialization complete.\n'
-}
-
 config_toolkit()
 {
-    local archguard_root="$AG_HOME/.archguard"
+    local toolkit_root="$AG_HOME/.archguard/toolkit"
+    local configs_destination="$toolkit_root/archguard-configs"
+
     local toolkit_repo="https://github.com/WillemAchterhof/archguard-toolkit.git"
-    local toolkit_destination="$archguard_root/toolkit"
     local configs_repo="https://github.com/WillemAchterhof/archguard-configs.git"
-    local configs_destination="$archguard_root/archguard-configs"
 
     printf '[*] Installing ArchGuard Toolkit...\n'
 
-    mkdir -p -- "$archguard_root"
+    mkdir -p -- "$toolkit_root"
 
     git clone \
         "$toolkit_repo" \
-        "$toolkit_destination"
+        "$toolkit_root"
 
     printf '[*] Installing ArchGuard Configs...\n'
 
